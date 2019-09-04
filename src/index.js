e6764dc2420de123b32ff1c1cd7142b7d25b2844
Dmitry Gavrilov <ds33vladimir@yandex.ru>
2019-09-04 07:38:25 +0300

    Edit error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import "../src/style.css";
+import "./style.css";
 import '../images/close.svg';
-import '../images/logo.svg';
+import './images/logo.svg';
 import {placesList} from './card.js';
 import {api, usName, usJob} from './api.js';
 import {handleValidate} from './validation.js';
